7a159fab0de7ffd80ca8982992b80606d63b1542
JuanPabloVerduzcoCampos <jverduzco26@ucol.mx>
2022-10-23 13:20:17 -0500

    final

--- Inventario.js
+++ Inventario.js
@@ -15,58 +15,58 @@ class Inventario {
             temp.siguiente = nuevo;
         }
     }
-    
-    buscar(nCodigo){
+
+    buscar(Codigo){
         let temp = this.inicio;
-        while (temp.codigo != nCodigo){
+        while (temp != null){
+            if (temp.Codigo === Codigo){
+                return temp;
+            }
             temp = temp.siguiente;
         }
-        if (temp.codigo === nCodigo){
-            return temp;
-        }
+        return null;
     }
-
-    eliminar(nCodigo) {
+    
+    eliminar(Codigo){
         let temp = this.inicio;
-        if (this.inicio.codigo === nCodigo){
-            this.inicio = this.inicio.siguiente;
-        } else {
-            while (temp.siguiente.codigo != nCodigo){
-                temp = temp.siguiente;
+        let anterior = null;
+        while (temp != null){
+            if (temp.codigo == Codigo){
+                if (anterior == null){
+                    this.inicio = temp.siguiente;
+                } else {
+                    anterior.siguiente = temp.siguiente;
+                }
+                return temp;
             }
-    
-            if (temp.siguiente.codigo === nCodigo){
-                temp.siguiente = temp.siguiente.siguiente;
-            } else {
-                alert ('Inexistente');
-            }
-        } 
+            anterior = temp;
+            temp = temp.siguiente;
+        }
+        return null;
     }
-    
 
     insertar(nuevo, posicion){
-        let temp = this.inicio;
-        if (posicion === 1){
-            this.agregarInicio(nuevo);
+        if (posicion == 0){
+            nuevo.siguiente = this.inicio;
+            this.inicio = nuevo;
+
         } else {
-            let i = 1;
-            while ((i + 1) != posicion){
-                if (temp.siguiente != null){
-                    temp = temp.siguiente;
-                    i++;
-                } else {
-                    alert('Inexistente');
-                    break;
-                }
-            } 
-    
-            if ((i+1) === posicion){
-                nuevo.siguiente = temp.siguiente;
-                temp.siguiente = nuevo;
+            let temp = this.inicio;
+            let anterior = null;
+            let i = 0;
+            while (temp != null && i < posicion){
+                anterior = temp;
+                temp = temp.siguiente;
+                i++;
+
+
             }
+            anterior.siguiente = nuevo;
+            nuevo.siguiente = temp;
         }
     }
-    
+
+
     listarP(){
         let concat = "";
         let temp = this.inicio;
@@ -76,31 +76,20 @@ class Inventario {
         }
         return concat;
     }
-    
+
+
     listadoInversa(){
         let temp = this.inicio;
-        let j = 1;
-        while(temp.siguiente != null){
-            temp = temp.siguiente;
-            j++;
+        let anterior = null;
+        let siguiente = null;
+        while (temp != null){
+            siguiente = temp.siguiente;
+            temp.siguiente = anterior;
+            anterior = temp;
+            temp = siguiente;
         }
-
-        let concat = "" + temp.nombre;
-        j = j - 1;
-        while (j > 0){
-            let i = 1;
-            temp = this.inicio;
-            while (i < j){
-                temp = temp.siguiente;
-                i++;
-            }
-            concat += " " + temp.nombre ;
-            j--;
-        }
-
-        return concat;
+        this.inicio = anterior;
     }
-
 }
 
 
@@ -144,4 +133,49 @@ function addProducto(){
     document.getElementById("codigo").value="";
     document.getElementById("cantidad").value="";
     document.getElementById("costo").value="";
+}
+
+function buscarX(){
+    let codigo = document.getElementById("codigo").value;
+    let producto = lista.buscar(codigo);
+    if(producto == null){
+        alert("El producto no existe");
+    }else{
+        alert("El producto es: " + producto.nombre + " " + producto.codigo + " " + producto.cantidad + " " + producto.costo);
+    }
+}
+
+
+function eliminarX(){
+    let codigo = document.getElementById("eliminar").value;
+    let producto = lista.eliminar(codigo);
+    console.log(producto);
+}
+
+function insertar(){
+    let producto = document.getElementById("nombre").value;
+    let codigo = document.getElementById("codigo").value;
+    let cantidad = document.getElementById("cantidad").value;
+    let costo = document.getElementById("costo").value;
+    let articulo = new Producto (producto, codigo, cantidad, costo);
+    lista.insertar(articulo, codigo);
+    console.log(lista);
+}
+
+
+function listar(){
+    let tabla = document.getElementById("productos");
+    let temp = lista.inicio;
+    while (temp != null){
+        let fila = tabla.insertRow(-1);
+        let celda1 = fila.insertCell(0);
+        let celda2 = fila.insertCell(1);
+        let celda3 = fila.insertCell(2);
+        let celda4 = fila.insertCell(3);
+        celda1.innerHTML = temp.nombre;
+        celda2.innerHTML = temp.codigo;
+        celda3.innerHTML = temp.cantidad;
+        celda4.innerHTML = temp.costo;
+        temp = temp.siguiente;
+    }
 }
